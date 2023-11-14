022ef896f576c2b059bd69d033d3b0fb6ee6210b
Zain Basit <45300116+Zain-Basit@users.noreply.github.com>
2023-11-14 17:48:47 -0500

    Add animals tag, separate categories, and bug fixs
    Animals category was added to the CATEGORIES array
    by special request, additionally I removed the
    array from the App.js file and put it into a
    separate file.
    
    I'll probably move that to a fixtures or static
    data folder once I remove the rest of the
    components from App.js and place them into
    other folders.
    
    Bug Fix:
    Removed a reference to manifest.json in the
    index.html file, it was causing issues since
    the file does not exist.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import { useState, useEffect } from "react";
-import supabase from "./supabase";
-
 import "./styles.css";
-export default App;
+import supabase from "./supabase";
+import CATEGORIES from "./Categories";
 
-const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
-];
+export default App;
 
 function App() {
   // Defining State Variables
---
+++ src/Categories.js
@@ -0,0 +1,13 @@
+const CATEGORIES = [
+    { name: "technology", color: "#3b82f6" },
+    { name: "science", color: "#16a34a" },
+    { name: "finance", color: "#ef4444" },
+    { name: "society", color: "#eab308" },
+    { name: "entertainment", color: "#db2777" },
+    { name: "health", color: "#14b8a6" },
+    { name: "history", color: "#f97316" },
+    { name: "news", color: "#8b5cf6" },
+    { name: "animals", color: "#bebebe" }
+];
+
+export default CATEGORIES;
