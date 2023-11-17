0d35c0564b1bf4e00d0bdd13458857ab3a041064
Zain Basit <45300116+Zain-Basit@users.noreply.github.com>
2023-11-17 02:01:07 -0500

    Formatting changes and include a linter/formatter
    Configured eslint and prettier along with a pre-commit
    hook to keep the code formatted. Additionally modified
    the test file for Category.test.js a bit.

--- src/components/Category/Category.test.js
+++ src/components/Category/Category.test.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { render, screen, cleanup } from "@testing-library/react";
-import Category from "./Category";
+import React from 'react';
+import { render, screen, cleanup } from '@testing-library/react';
+import Category from './Category';
 
-var mockCategory;
+let mockCategory;
 
-describe("Category", () => {
+describe('Category', () => {
   beforeEach(() => {
-    mockCategory = { name: "technology", color: "#3b82f6" };
+    mockCategory = { name: 'technology', color: '#3b82f6' };
   });
 
   afterEach(() => {
     cleanup();
   });
 
-  test("should render on the document", () => {
+  test('should render on the document', () => {
     render(<Category category={mockCategory} />);
     const cat = screen.getByTestId(`category-${mockCategory.name}`);
     expect(cat).toBeInTheDocument();
